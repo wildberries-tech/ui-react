2ff4bede21b6e727f3ac2f5ca88209235c586ecd
lamo2k123 <lamo2k123@gmail.com>
2022-12-07 22:58:27 +0300

    Реализация компонента Button closed #19

--- src/components/button/stories/index.stories.tsx
+++ src/components/button/stories/index.stories.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { IconVideo } from './../../icons/video';
