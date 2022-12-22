6017e131fefe2cb163f61d64f4cc818209958ea4
lamo2k123 <lamo2k123@gmail.com>
2022-12-22 08:42:19 +0000

    Deploying to gh-pages from @ wildberries-tech/ui-react@1f21be11a1621bbc722759ff8d6dee5cfd319fb2 🚀

---
+++ 745.74bce9bc.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_wb_tech_ui_react=self.webpackChunk_wb_tech_ui_react||[]).push([[745],{"./node_modules/react-dom/client.js":(__unused_webpack_module,exports,__webpack_require__)=>{var m=__webpack_require__("./node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
