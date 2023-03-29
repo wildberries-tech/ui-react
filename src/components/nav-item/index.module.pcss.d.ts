e9bb60793f92d2b42325d6a537f60e610cf7b62e
lamo2k123 <lamo2k123@gmail.com>
2023-03-29 13:16:29 +0300

    Add nav item

---
+++ src/components/nav-item/index.module.pcss.d.ts
@@ -0,0 +1,12 @@
+/* This file was automatically generated. */
+declare namespace IndexModulePcssNamespace {
+  export interface IIndexModulePcss {
+    "nav-item": string;
+    "nav-item_disabled": string;
+    "nav-item_preset-active": string;
+  }
+}
+
+declare const IndexModulePcssModule: IndexModulePcssNamespace.IIndexModulePcss;
+
+export = IndexModulePcssModule;
