1d95538d00f9060f2053cc5df0ff539d2d443989
lamo2k123 <lamo2k123@gmail.com>
2023-04-19 11:37:51 +0300

    Добавлены отступы в tabs-panel (section и page)

--- packages/ui-react/src/components/tabs/components/tab-panel/index.module.pcss.d.ts
+++ packages/ui-react/src/components/tabs/components/tab-panel/index.module.pcss.d.ts
@@ -3,7 +3,9 @@
 
 interface Stylesheet {
     'tab-panel': string
+    'tab-panel_preset-page': string
     'tab-panel_preset-pane': string
+    'tab-panel_preset-section': string
     'tab-panel_selected': string
 }
 
