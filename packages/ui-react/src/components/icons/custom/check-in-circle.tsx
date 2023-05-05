8fddef2caa40c325749e0beadcae4c0e3d040ea9
kritua <kritua2@gmail.com>
2023-05-05 17:13:02 +0300

    Custom icons top line

---
+++ packages/ui-react/src/components/icons/custom/check-in-circle.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { SVG, IProps as IPropsSVG } from '..';
+
+export interface IProps {
+    svg?: IPropsSVG
+}
+
+export const IconCheckInCircle = (props: IProps) => (
+    <SVG {...props.svg}>
+        <circle
+            cx="12"
+            cy="12"
+            r="12"
+            fill="#067B00"
+        />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9.56309 15.1999L18.6215 6L20 7.40006L9.56309 18L4 12.35L5.37853 10.95L9.56309 15.1999Z"
+            fill="white"
+        />
+    </SVG>
+);
+
+export default IconCheckInCircle;
