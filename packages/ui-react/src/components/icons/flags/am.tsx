1b651ee0266195104aaf08e9a09d971c0470a1f2
lamo2k123 <lamo2k123@gmail.com>
2023-09-05 23:24:23 +0300

    feat(Icons): Added flags AM, BY, KG, KZ, RU, UZ

---
+++ packages/ui-react/src/components/icons/flags/am.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { SVG, IProps as IPropsSVG } from '..';
+
+export interface IProps {
+    svg?: IPropsSVG
+}
+
+export const IconFlagsAM = (props: IProps) => (
+    <SVG
+        {...props.svg}
+        viewBox="0 0 640 480"
+    >
+        <path
+            fill="#d90012"
+            d="M0 0h640v160H0z"
+        />
+        <path
+            fill="#0033a0"
+            d="M0 160h640v160H0z"
+        />
+        <path
+            fill="#f2a800"
+            d="M0 320h640v160H0z"
+        />
+    </SVG>
+);
+
+export default IconFlagsAM;
