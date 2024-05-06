6491b8bccbcaf185157a5c85f224bea7ec135493
Alena Shpakova <82038731+alena-shpakova@users.noreply.github.com>
2024-05-06 11:03:49 +0400

    task(AP-2562): add CheckboxIcon

---
+++ packages/ui-react/src/components/checkbox/v2/checkbox-icon/index.module.pcss.d.ts
@@ -0,0 +1,14 @@
+
+
+
+interface Stylesheet {
+    'checkbox-icon': string
+    'checkbox-icon__check': string
+    'checkbox-icon__disabled': string
+    'checkbox-icon__indeterminate': string
+    'checkbox-icon__rect': string
+}
+
+
+declare const stylesheet: Stylesheet
+export default stylesheet
---
+++ packages/ui-react/src/components/checkbox/v2/checkbox-icon/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { SVG } from '../../../icons';
+import { useClassnames } from '../../../../hooks/use-classnames';
+
+import style from './index.module.pcss';
+
+export interface IProps {
+    readonly checked?: boolean,
+    readonly indeterminate?: boolean,
+    readonly disabled?: boolean
+}
+
+export const CheckboxIcon = (props: IProps) => {
+    const cn = useClassnames(style);
+
+    return (
+        <SVG
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            className={cn('checkbox-icon')}
+        >
+            <path
+                className={cn('checkbox-icon__rect')}
+                d="M2.223 4.741a2.968 2.968 0 0 1 2.518-2.518 23.042 23.042 0 0 1 6.518 0 2.968 2.968 0 0 1 2.518 2.518 23.049 23.049 0 0 1 0 6.518 2.968 2.968 0 0 1-2.518 2.518 23.049 23.049 0 0 1-6.518 0 2.968 2.968 0 0 1-2.518-2.518 23.042 23.042 0 0 1 0-6.518Z"
+            />
+            {props.indeterminate ? (
+                <rect
+                    className={cn('checkbox-icon__indeterminate')}
+                    width="6"
+                    height="2"
+                    x="5"
+                    y="7"
+                    rx="1"
+                />
+            ) : null}
+            <path
+                className={cn('checkbox-icon__check')}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m5 7.8 2.3 2.3 4.271-4.6"
+            />
+            {props.disabled && !props.checked && !props.indeterminate ? (
+                <path
+                    className={cn('checkbox-icon__disabled')}
+                    d="m3 3 10 10"
+                />
+            ) : null}
+        </SVG>
+    );
+};
